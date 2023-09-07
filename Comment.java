2a4d56bd97ef73292ce2b38d85763252763d4811
JosephBasipaka <141002779+JosephBasipaka@users.noreply.github.com>
2023-09-07 09:05:39 +0530

    comments

---
+++ Comment.java
@@ -0,0 +1,88 @@
+package com.prodapt.learningspring.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "comments")
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "postId", referencedColumnName = "id")
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name = "parentId", referencedColumnName = "id")
+    private Comment parent;
+    
+    
+
+	public Comment() {
+		super();
+	}
+
+	public Comment(long id, String content, User user, Post post, Comment parent) {
+		super();
+		this.id = id;
+		this.content = content;
+		this.user = user;
+		this.post = post;
+		this.parent = parent;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	public Comment getParent() {
+		return parent;
+	}
+
+	public void setParent(Comment parent) {
+		this.parent = parent;
+	}
+    
+    
+}
